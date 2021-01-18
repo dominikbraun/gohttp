ad5e78f2174e7e8ef9a571287e1db0531e0bb784
Dominik Braun <mail@dominikbraun.io>
2021-01-18 21:40:17 +0100

    Add tests for parseRequestLine and parseStatusLine

--- gohttp_test.go
+++ gohttp_test.go
@@ -8,17 +8,17 @@ import (
 	"testing"
 )
 
-type message struct {
-	method   string
-	url      string
-	protocol string
-	body     string
-}
-
 // TestParseRequest tests the ParseRequest function by providing sample
 // requests with different HTTP methods. The parsed http.Request instance
 // is expected to possess the same data as the message struct.
 func TestParseRequest(t *testing.T) {
+	type message struct {
+		method   string
+		url      string
+		protocol string
+		body     string
+	}
+
 	testCases := map[string]struct {
 		source   string
 		expected message
@@ -96,6 +96,92 @@ func TestSerializeRequest(t *testing.T) {
 
 		if string(actual) != tc.expected {
 			t.Errorf("'%s': expected request %s, got %s", name, tc.expected, string(actual))
+		}
+	}
+}
+
+func TestParseResponse(t *testing.T) {}
+
+func TestSerializeResponse(t *testing.T) {}
+
+func TestParseRequestLine(t *testing.T) {
+	type requestLine struct {
+		method    string
+		parsedURL string
+		protocol  string
+	}
+
+	testCases := map[string]struct {
+		line     string
+		expected requestLine
+	}{
+		"GET request": {
+			line: "GET example.com HTTP/1.1",
+			expected: requestLine{
+				method:    "GET",
+				parsedURL: "example.com",
+				protocol:  "HTTP/1.1",
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		actualMethod, actualURL, actualProtocol, err := parseRequestLine(tc.line)
+		if err != nil {
+			t.Fatalf("'%s': unexpected error: %s", name, err.Error())
+		}
+
+		if actualMethod != tc.expected.method {
+			t.Errorf("'%s': expected method %s, got %s", name, tc.expected.method, actualMethod)
+		}
+
+		if actualURL.String() != tc.expected.parsedURL {
+			t.Errorf("'%s': expected URL %s, got %s", name, tc.expected.method, actualMethod)
+		}
+
+		if actualProtocol != tc.expected.protocol {
+			t.Errorf("'%s': expected protocol %s, got %s", name, tc.expected.method, actualMethod)
+		}
+	}
+}
+
+func TestParseStatusLine(t *testing.T) {
+	type statusLine struct {
+		protocol     string
+		statusCode   int
+		reasonPhrase string
+	}
+
+	testCases := map[string]struct {
+		line     string
+		expected statusLine
+	}{
+		"GET request": {
+			line: "HTTP/1.1 200 OK",
+			expected: statusLine{
+				protocol:     "HTTP/1.1",
+				statusCode:   200,
+				reasonPhrase: "OK",
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		actualProtocol, actualStatusCode, actualReasonPhrase, err := parseStatusLine(tc.line)
+		if err != nil {
+			t.Fatalf("'%s': unexpected error: %s", name, err.Error())
+		}
+
+		if actualProtocol != tc.expected.protocol {
+			t.Errorf("'%s': expected protocol %s, got %s", name, tc.expected.protocol, actualProtocol)
+		}
+
+		if actualStatusCode != tc.expected.statusCode {
+			t.Errorf("'%s': expected status code %d, got %d", name, tc.expected.statusCode, actualStatusCode)
+		}
+
+		if actualReasonPhrase != tc.expected.reasonPhrase {
+			t.Errorf("'%s': expected reason phrase %s, got %s", name, tc.expected.reasonPhrase, actualReasonPhrase)
 		}
 	}
 }
